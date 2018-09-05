c6378ba31d13c2220d7e79524cb9560854b33660
Eagan Martin <mece.martinece@gmail.com>
2018-09-05 18:17:12 +0800

    Modified main activity for switching to expanded layout;

--- app/src/main/java/com/fujiyama/pulp/responsiveprofileui/MainActivity.java
+++ app/src/main/java/com/fujiyama/pulp/responsiveprofileui/MainActivity.java
@@ -1,9 +1,15 @@
 package com.fujiyama.pulp.responsiveprofileui;
 
+import android.support.constraint.ConstraintLayout;
+import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private boolean isOpen = false;
+    private ConstraintSet mainLayout, expandedLayout;
+    private ConstraintLayout constraintLayout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
