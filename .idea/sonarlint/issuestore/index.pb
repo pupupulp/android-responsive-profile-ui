a980d734a602ea58f0f4331e281a2cf5426cc402
Eagan Martin <mece.martinece@gmail.com>
2018-09-05 19:02:58 +0800

    Transition for expanded UI;

---
+++ .idea/sonarlint/issuestore/index.pb
@@ -0,0 +1,13 @@
+
+R
+"app/src/main/res/values/styles.xml,f/7/f79cc2688e29fcbc286fda14ae07b27c9e83690f
+R
+"app/src/main/res/values/colors.xml,9/6/969faa2a5d9f5e8390e958d2c07f2cfd54676e6b
+@
+app/build.gradle,f/4/f4a01d6a4fcb971362ec00a83903fd3902f52164
+\
+,app/src/main/res/layout/expanded_profile.xml,2/5/257b4ffde4e12060df798886c6dbcde25cd39a1c
+y
+Iapp/src/main/java/com/fujiyama/pulp/responsiveprofileui/MainActivity.java,a/8/a80874d7ef171f384a7bfa619480b0e6a86c811c
+Y
+)app/src/main/res/layout/activity_main.xml,7/6/76b07e40f24cce093536731477540f12e7da3cb6
--- app/src/main/java/com/fujiyama/pulp/responsiveprofileui/MainActivity.java
+++ app/src/main/java/com/fujiyama/pulp/responsiveprofileui/MainActivity.java
@@ -4,16 +4,47 @@ import android.support.constraint.ConstraintLayout;
 import android.support.constraint.ConstraintSet;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.transition.TransitionManager;
+import android.view.View;
+import android.view.Window;
+import android.view.WindowManager;
+import android.widget.ImageView;
 
 public class MainActivity extends AppCompatActivity {
 
     private boolean isOpen = false;
     private ConstraintSet mainLayout, expandedLayout;
     private ConstraintLayout constraintLayout;
+    private ImageView imageViewProfileCover;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        Window w = getWindow();
+        w.setFlags(WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS, WindowManager.LayoutParams.FLAG_LAYOUT_NO_LIMITS);
+
+        mainLayout = new ConstraintSet();
+        expandedLayout = new ConstraintSet();
+        imageViewProfileCover = findViewById(R.id.profileCover);
+        constraintLayout = findViewById(R.id.constraintLayout);
+
+        mainLayout.clone(constraintLayout);
+        expandedLayout.clone(this, R.layout.expanded_profile);
+
+        imageViewProfileCover.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View view) {
+                if(!isOpen) {
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    expandedLayout.applyTo(constraintLayout);
+                    isOpen = !isOpen;
+                } else {
+                    TransitionManager.beginDelayedTransition(constraintLayout);
+                    mainLayout.applyTo(constraintLayout);
+                    isOpen = !isOpen;
+                }
+            }
+        });
     }
 }
